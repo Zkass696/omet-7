3038fdd94363055eb763257854b81082c290a051
Zkass696 <hauzaan9set@gmail.com>
2026-05-18 09:19:35 +0700

    Add files via upload

---
+++ database.js
@@ -0,0 +1,54 @@
+const OMET_DB = {
+    // FORMAT: "username": { pass, nama, jabatan, level }
+    
+    "Ketua osis": { 
+        pass: "Inti7Ketos", 
+        nama: "Annisa Naifa Ramadhani", 
+        jabatan: "Ketua OSIS", 
+        level: "admin" 
+    },
+    "Wakil Ketua 1": { 
+        pass: "Inti7Wkt1", 
+        nama: "Keyroe Indra Muhammad", 
+        jabatan: "Wakil Ketua OSIS 1", 
+        level: "admin" 
+    },
+    "Wakil Ketua 2": { 
+        pass: "Inti7Wkt2", 
+        nama: "Adhelia Destiyana Rahayu",
+        jabatan: "Wakil Ketua OSIS 2", 
+        level: "admin" 
+    },
+    "sekretaris 1": {
+        pass: "Inti7Skr1",
+        nama: "Dewi Damayanti Suparmo",
+        jabatan: "Sekretaris 1",
+        level: "admin"
+    },
+    "sekretaris 2": {
+        pass: "Inti7Skr2",
+        nama: "Liby Alyafie Putri",
+        jabatan: "Sekretaris 2",
+        level: "admin"
+    },
+    "bendahara 1": { 
+        pass: "Inti7Bnd1", 
+        nama: "Roro Keisya Aufa Az-Zaura", 
+        jabatan: "Bendahara 1", 
+        level: "admin" 
+    },
+    "bendahara 2": {
+        pass: "Inti7Bnd2",
+        nama: "Aqeela Ghina Nashita",
+        jabatan: "Bendahara 2",
+        level: "admin"
+    },
+    
+    "sekbid 1": { 
+        pass: "anggota_skb1", 
+        nama: "Dika Pratama", 
+        kelas: "7A", 
+        jabatan: "Anggota Sekbid", 
+        level: "user" 
+    }
+};
